5623d5e66d56042a33c52409afc6807bf478f480
boliyou <2454462834@qq.com>
2022-11-15 02:31:03 +0800

    feat: 优化rollup和package.json配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,18 @@ export default {
   input: 'src/index.js',
   output: [
     {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      plugins: [terser()],
+    },
+    {
       file: 'dist/index.es.js',
       format: 'es',
       plugins: [terser()],
     },
     {
-      file: 'dist/index.umd.js',
-      format: 'umd',
+      file: 'dist/index.iife.js',
+      format: 'iife',
       name: '$y',
       plugins: [terser()],
     },
