9f86aa1440d90cee84ed6a3b343fcf80cfc82936
boliyou <2454462834@qq.com>
2022-11-15 03:31:18 +0800

    refactor: 去除无效的字段，确定此包为esmodule版本

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,9 @@ import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      plugins: [terser()],
-    },
-    {
-      file: 'dist/index.es.js',
-      format: 'es',
-      plugins: [terser()],
-    },
-    {
-      file: 'dist/index.iife.js',
-      format: 'iife',
-      name: '$y',
-      plugins: [terser()],
-    },
-  ],
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    plugins: [terser()],
+  },
 }
