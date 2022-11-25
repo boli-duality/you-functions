d7747e10a0f8dff6758af42943294aa2acf5398d
youbolin <youbolin001@163.com>
2022-11-25 15:19:41 +0800

    perf: myset.pototype.radio性能小优化

--- src/index.js
+++ src/index.js
@@ -340,7 +340,7 @@ export class MySet {
   }
   // 判断是否存在元素
   has(v) {
-    return !!~this.value.indexOf(v)
+    return this.value.includes(v)
   }
   // 添加元素
   add(v) {
@@ -363,7 +363,7 @@ export class MySet {
   }
   // 单选必选一个
   radio(v) {
-    this.value.splice(0, this.value.length, v)
+    if (!this.value.includes(v)) this.value.splice(0, this.value.length, v)
   }
   // 单选可不选
   radioToggle(v) {
