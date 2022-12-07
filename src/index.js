095be4a2afb7102f4d17307afc15ef27956c3036
youbolin <youbolin001@163.com>
2022-12-07 17:06:44 +0800

    feat: 新增logs方法

--- src/index.js
+++ src/index.js
@@ -554,3 +554,18 @@ export const safeArr = function (origin, chain) {
   if (!Array.isArray(chain)) return []
   return result
 }
+
+/**
+ * 带颜色标签的console.log
+ * @param {string|{label:string;style:string}} opts label的配置
+ * @param  {...any} args 要打印的内容
+ */
+export const logs = (opts, ...args) => {
+  const defaultStyle = 'color:white;background:green;padding:2px 5px;border-radius:4px'
+  if (typeof opts == 'string') console.log(`%c${opts}`, defaultStyle, ...args)
+  else if (!opts) console.log(...args)
+  else if (typeof opts == 'object') {
+    const { label, style } = opts
+    console.log(`%c${label}`, style, ...args)
+  }
+}
