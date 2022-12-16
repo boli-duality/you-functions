dd53e79af7ddb98b41a8e93818f1114c1c3bef66
youbolin <youbolin001@163.com>
2022-12-16 10:26:51 +0800

    chore: 修改jsdoc漏网之鱼

--- src/initDebounce.js
+++ src/initDebounce.js
@@ -1,9 +1,9 @@
 /**
  * vue版的防抖函数初始化函数
  * @author 游博林 @2022-2-16 01:42
- * @property {Number} delay = 500       默认防抖延迟时间
- * @property {Boolean} immediate = true 是否立即执行
- * @returns {Function} debounce vue版的防抖函数
+ * @property {number} delay = 500       默认防抖延迟时间
+ * @property {boolean} immediate = true 是否立即执行
+ * @returns {function} debounce vue版的防抖函数
  */
 function initDebounce({ delay = 600, immediate = false } = {}) {
   const defaultDelay = delay
@@ -12,12 +12,12 @@ function initDebounce({ delay = 600, immediate = false } = {}) {
   const timeoutID = {}
   /**
    * vue版的防抖函数
-   * @param {Function} fn                     需要防抖的函数
-   * @param {Object} options                  配置选项
-   * @property {Array} params of options      需要防抖的函数的参数
-   * @property {Number} delay of options      防抖延迟时间
-   * @property {Boolean} immediate of options 是否立即执行
-   * @property {String|Number} id of options  传入id启用不同的防抖函数
+   * @param {function} fn                     需要防抖的函数
+   * @param {object} options                  配置选项
+   * @property {array} params of options      需要防抖的函数的参数
+   * @property {number} delay of options      防抖延迟时间
+   * @property {boolean} immediate of options 是否立即执行
+   * @property {string|number} id of options  传入id启用不同的防抖函数
    */
   return function debounce(fn, { params, delay, immediate, id = ID } = {}) {
     if (typeof fn !== 'function') {
--- src/initThrottle.js
+++ src/initThrottle.js
@@ -1,9 +1,9 @@
 /**
  * vue版的节流函数初始化函数
  * @author 游博林 @2022-2-15 18:56
- * @property {Number} delay = 500       默认节流延迟时间
- * @property {Boolean} immediate = true 是否立即执行
- * @returns {Function} vue版的节流函数
+ * @property {number} delay = 500       默认节流延迟时间
+ * @property {boolean} immediate = true 是否立即执行
+ * @returns {function} vue版的节流函数
  */
 function initThrottle({ delay = 500, immediate = true } = {}) {
   const defaultDelay = delay
@@ -12,12 +12,12 @@ function initThrottle({ delay = 500, immediate = true } = {}) {
   const isDoThrottle = { [ID]: false }
   /**
    * vue版的节流函数
-   * @param {Function} fn                     需要节流的函数
-   * @param {Object} options                  配置选项
-   * @property {Array} params of options      需要节流的函数的参数
-   * @property {Number} delay of options      节流延迟时间
-   * @property {Boolean} immediate of options 是否立即执行
-   * @property {String|Number} id of options  传入id启用不同的节流函数
+   * @param {function} fn                     需要节流的函数
+   * @param {object} options                  配置选项
+   * @property {array} params of options      需要节流的函数的参数
+   * @property {number} delay of options      节流延迟时间
+   * @property {boolean} immediate of options 是否立即执行
+   * @property {string|number} id of options  传入id启用不同的节流函数
    */
   return function throttle(fn, { params, delay, immediate, id = ID } = {}) {
     if (typeof fn !== 'function') {
